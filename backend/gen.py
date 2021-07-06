6d47d30a3fe36582158aa70f5e7b53908f0d5022
Hasan Parasteh <hasanparasteh@gmail.com>
2021-07-06 14:56:31 +0430

    fix format issue at backend

--- backend/gen.py
+++ backend/gen.py
@@ -1,8 +1,11 @@
 import os
-
-from flask import Flask, jsonify, request
+from datetime import datetime
+from flask import Flask, jsonify, request, send_file
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 PATH = os.getcwd()
 
@@ -11,15 +14,36 @@ PATH = os.getcwd()
 def starmap():
     content = request.json
 
+    # Format Coord
     coord = content["coord"]
+    # Format Time
     time = content["time"]
+    time = time.split(":")
+    if len(time) == 2:
+        time.append("00")
+    time = ".".join(time)
+    # Format Date
     date = content["date"]
+    date_obj = datetime.strptime(date, '%Y-%m-%d')
+    date = date_obj.strftime("%d.%m.%Y")
+    # Format City
     city = content["city"]
+    # Format Filename
     filename = content["name"] + ".svg"
+    # Format Info
+    info = content["info"]
 
     os.system(
-        f"python starmap.py -coord {coord} -time {time} -date {date} -utc +3 -guides False -constellation True -o {'images/'+ filename} -info {city}")
-    return jsonify(result=True, message=f"Created")
+        f"python starmap.py -coord {coord} -time {time} -date {date} -utc +3 -constellation True -o {'images/'+ filename} -info {city}")
+
+    response = jsonify(result=True, message="Starmap created successfully",
+                       path=f"http://{request.host}/download/{filename}")
+    return response
+
+
+@app.route('/download/<path>')
+def downloadFile(path):
+    return send_file("images/" + path, as_attachment=True)
 
 
 @app.route("/")
---
+++ frontend/js/app.js
@@ -0,0 +1,46 @@
+import 'regenerator-runtime/runtime';
+import axios from 'axios';
+
+const SERVER = "http://127.0.0.1:5000"
+
+
+const form = document.getElementById('form');
+form.onsubmit = submit;
+
+function submit(event) {
+    event.preventDefault();
+    CerateSVG()
+}
+
+function CerateSVG() {
+    const coord = document.getElementById("coord").value
+    const time = document.getElementById("time").value
+    const date = document.getElementById("date").value
+    const city = document.getElementById("city").value
+    const info = document.getElementById("info").value
+
+    const data = {
+        coord: coord,
+        time: time,
+        date: date,
+        city: city,
+        info: info,
+        name: uuidv4()
+    }
+
+    const svg = document.querySelector("body");
+    const image = document.createElement("img");
+
+    axios.post(SERVER + '/starmap', data).then(response => {
+        image.src = response.data.path;
+        svg.appendChild(image);
+    })
+}
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
