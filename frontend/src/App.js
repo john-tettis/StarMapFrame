b2648a44a2714a7b8621185f706710b319b71d82
Hasan Parasteh <hasanparasteh@gmail.com>
2021-07-14 11:33:14 +0430

    added react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Landing/Home";
 import StarDrawer from "./components/StarDrawer/StarDrawer";
-import './assets/css/styles.css';
+import "./assets/css/styles.css";
 
 export default function App() {
-    return (
-        <React.Fragment>
-            <StarDrawer/>
-        </React.Fragment>
-    );
+  return (
+      <Router>
+        <div>
+          <nav className="navbar navbar-light bg-light">
+            <ul className="nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">صفحه‌ی اصلی</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/star/">ستاره‌ساز</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/star/">
+              <StarDrawer/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+  );
 }
-
---
+++ frontend/src/components/Landing/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div className="container">
+            <div className="row my-5">
+                <div className="col-12">
+                صفحه‌ی اصلی
+                </div>
+            </div>
+        </div>
+    );
+}
