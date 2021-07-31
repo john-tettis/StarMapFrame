41e6189469fde9426d9148c83de06e9f5a84bf42
Hasan Parasteh <hasanparasteh@gmail.com>
2021-07-31 15:10:04 +0430

    fixed login button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -22,7 +22,7 @@
             ستاره ساز
           </router-link>
         </li>
-        <li v-if="$cookies.isKey('token') && $cookies.get('token') === null">
+        <li v-if="!$cookies.isKey('token') && $cookies.get('token') === null">
           <router-link class="white--text" to="/login">
             ورود
           </router-link>
