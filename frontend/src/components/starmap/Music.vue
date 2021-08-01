a49efea74c56b542777b2b1ec8e86b4242acdf1d
Hasan Parasteh <hasanparasteh@gmail.com>
2021-08-01 12:47:57 +0430

    remove singer & name from form & take action on another button

--- frontend/src/components/starmap/Music.vue
+++ frontend/src/components/starmap/Music.vue
@@ -17,53 +17,46 @@
     ></v-switch>
     <v-form v-if="wantMusic" ref="form" v-model="valid">
       <v-file-input
+        ref="mp3"
         v-model="mp3"
         :show-size="1000"
         accept="audio/*"
         label="فایل موزیک"
         outlined
         dense
+        :clearable="false"
+        append-outer-icon="mdi-close-box-outline"
+        @click:append-outer="mp3=[]"
       ></v-file-input>
       <v-file-input
+        ref="cover"
         v-model="cover"
         :show-size="1000"
         accept="image/*"
         label="فایل کاور"
         outlined
         dense
+        :clearable="false"
+        append-outer-icon="mdi-close-box-outline"
+        @click:append-outer="cover=[]"
       ></v-file-input>
-      <v-text-field dense outlined v-model="singer" label="خواننده" />
-      <v-text-field dense outlined v-model="name" label="نام موسیقی" />
-      <v-row>
-        <v-col>
-          <v-btn
-            block
-            class="my-5 float-end"
-            color="secondary"
-            @click="uploadMusic"
-          >
-            آپلود موسیقی
-          </v-btn>
-        </v-col>
-      </v-row>
     </v-form>
     <v-row>
-        <v-col cols="12" xl="6" lg="6" md="12" sm="12">
-          <v-btn
-            @click="$emit('update:stepper', 2)"
-            color="error"
-            block
-            outlined
-            >مرحله‌ی قبلی</v-btn
-          >
-        </v-col>
-        <v-col cols="12" xl="6" lg="6" md="12" sm="12">
-          <v-btn color="primary" block @click="$emit('update:stepper', 4)"
-            >مرحله‌ی بعدی</v-btn
-          >
-        </v-col>
-      </v-row>
-      
+      <v-col cols="12" xl="6" lg="6" md="12" sm="12">
+        <v-btn @click="$emit('update:stepper', 2)" color="error" block outlined
+          >مرحله‌ی قبلی</v-btn
+        >
+      </v-col>
+      <v-col cols="12" xl="6" lg="6" md="12" sm="12">
+        <v-btn
+          :disabled="wantMusic && mp3.length === 0 || cover.length===0"
+          color="primary"
+          block
+          @click="uploadMusic"
+          >مرحله‌ی بعدی</v-btn
+        >
+      </v-col>
+    </v-row>
     <Loading :isLoading="loading" />
   </div>
 </template>
@@ -83,50 +76,50 @@ export default {
       valid: false,
       mp3: [],
       cover: [],
-      singer: "",
-      name: "",
     };
   },
   methods: {
     uploadMusic() {
       this.loading = true;
-      const formData = new FormData();
-      formData.append("music", this.mp3);
-      formData.append("cover", this.cover);
-      formData.append("singer", this.singer);
-      formData.append("name", this.name);
-      formData.append("description", "");
-      formData.append("password", "respina1234");
-      this.axios
-        .post("https://respina.store/player/uploader.php", formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        })
-        .then((response) => {
-          if (response.data.result) {
-            this.$store.commit("setMusic", { qr: response.data.details.qr });
-            setTimeout(() => {
-              this.axios
-                .post("/api/starmap", this.$store.state.starmap)
-                .then((response) => {
-                  if (response.data.result) {
-                    this.$store.commit(
-                      "setImage",
-                      response.data.path + `?${Date.now()}`
-                    );
-                  }
-                  this.loading = false;
-                })
-                .catch((error) => {
-                  this.loading = false;
-                  console.log(error);
-                });
-            }, 500);
-          } else {
-            alert("something wrong happend");
-          }
-        });
+      if (this.wantMusic) {
+        const formData = new FormData();
+        formData.append("music", this.mp3);
+        formData.append("cover", this.cover);
+        formData.append("password", "respina1234");
+        this.axios
+          .post("https://respina.store/player/uploader.php", formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((response) => {
+            if (response.data.result) {
+              this.$store.commit("setMusic", { qr: response.data.details.qr });
+              setTimeout(() => {
+                this.axios
+                  .post("/api/starmap", this.$store.state.starmap)
+                  .then((response) => {
+                    if (response.data.result) {
+                      this.$store.commit(
+                        "setImage",
+                        response.data.path + `?${Date.now()}`
+                      );
+                      this.$emit("update:stepper", 4); // Moves to next step
+                    }
+                    this.loading = false;
+                  })
+                  .catch((error) => {
+                    this.loading = false;
+                    console.log(error);
+                  });
+              }, 500);
+            } else {
+              alert("something wrong happend");
+            }
+          });
+      } else {
+        this.$emit("update:stepper", 4); // Moves to next step
+      }
     },
   },
 };
