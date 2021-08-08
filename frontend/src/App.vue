07de590575c89c445242a83c0b1283e5cf46346c
Hasan Parasteh <hasanparasteh@gmail.com>
2021-08-08 13:00:58 +0430

    moves styles into main.scss

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -22,60 +22,10 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "@/assets/styles/main.scss";
+
 ::v-deep .v-toolbar__content {
   height: 45px;
-}
-ul.navbar {
-  list-style-type: none;
-}
-ul.navbar li {
-  display: inline;
-}
-ul.navbar a {
-  text-decoration: none;
-  padding: 11px 25px;
-  font-weight: bold;
-  border-radius: 5px;
-  font-size: 12px;
-  color: #fff;
-}
-ul.navbar a:hover {
-  background-color: hsl(0, 0%, 20%);
-}
-
-ul.mobile {
-  display: block !important;
-  z-index: 99999;
-  background-color: #eee;
-  position: relative;
-  top: 84px;
-  width: 100%;
-  padding-left: 0 !important;
-  box-shadow: 1px 0 10px rgba(0, 0, 0, 0.4);
-  border-radius: 0 0 5px 5px;
-}
-
-ul.mobile > li {
-  display: block;
-  width: 100%;
-  text-align: center;
-  margin: 10px auto;
-}
-ul.mobile a {
-  color: #212121 !important;
-  text-align: center;
-}
-.menu-icon {
-  display: none !important;
-}
-
-@media screen and (max-width: 990px) {
-  ul.navbar {
-    display: none;
-  }
-  .menu-icon {
-    display: block !important;
-  }
 }
 </style>
