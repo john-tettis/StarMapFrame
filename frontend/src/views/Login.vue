99dda8012e3b3501497563ec18ffdc75a4b0f6e0
Hasan Parasteh <hasanparasteh@gmail.com>
2021-08-09 19:21:32 +0430

    enable login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -115,18 +115,18 @@ export default {
   },
   methods: {
     doRegister() {
-      // this.axios.post("/api/register", this.register).then((response) => {
-      //   if (response.status === 200 && response.data.result) {
-      //     this.$router.push("/builder");
-      //   } else {
-      //     this.registerAlert = true;
-      //     this.registerAlertText = response.data.message;
-      //     setTimeout(() => {
-      //       this.registerAlert = false;
-      //     }, 5000);
-      //   }
-      // });
-      alert("ثبت نام غیر فعال است");
+      this.axios.post("/api/register", this.register).then((response) => {
+        if (response.status === 200 && response.data.result) {
+          this.$router.push("/builder");
+        } else {
+          this.registerAlert = true;
+          this.registerAlertText = response.data.message;
+          setTimeout(() => {
+            this.registerAlert = false;
+          }, 5000);
+        }
+      });
+      // alert("ثبت نام غیر فعال است");
     },
     doLogin() {
       this.axios.post("/api/login", this.login).then((response) => {
