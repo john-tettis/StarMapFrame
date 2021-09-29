f562a9209ab4c33d620b1c45267a2ec79cce7b23
Hasan Parasteh <hasanparasteh@gmail.com>
2021-09-29 12:15:08 +0330

    fix: music uploader for iphone users

--- frontend/src/components/starmap/Music.vue
+++ frontend/src/components/starmap/Music.vue
@@ -2,7 +2,7 @@
   <div>
     <v-row align="baseline">
       <v-col cols="12" xl="6" lg="6" md="12" sm="12">
-        <p class="p-title ">اضافه کردن بارکد موسیقی</p>
+        <p class="p-title">اضافه کردن بارکد موسیقی</p>
       </v-col>
     </v-row>
     <v-radio-group v-model="wantMusic">
@@ -18,20 +18,39 @@
     <div v-if="wantMusic">
       <v-form v-model="musicForm">
         <v-file-input
-        :rules="rules"
-        class="mb-0"
-        v-model="mp3"
-        :show-size="1000"
-        accept="audio/*"
-        label="فایل موزیک"
-        outlined
-        dense
-        :clearable="true"
-        @change="done = false"
-        hide-details
-      ></v-file-input>
+          v-if="getMobileOperatingSystem() === 'iOS'"
+          :rules="rules"
+          class="mb-0"
+          v-model="mp3"
+          :show-size="1000"
+          accept="audio/*"
+          label="فایل موزیک"
+          outlined
+          dense
+          :clearable="true"
+          @change="done = false"
+          hide-details
+        ></v-file-input>
+        <v-file-input
+          v-else
+          :rules="rules"
+          class="mb-0"
+          v-model="mp3"
+          :show-size="1000"
+          accept="*.mp3"
+          label="فایل موزیک"
+          outlined
+          dense
+          :clearable="true"
+          @change="done = false"
+          hide-details
+        ></v-file-input>
       </v-form>
-      <span style="text-align: center; color: gray;display:block" class="mt-2 mb-4">حداکثر سایز موسیقی ۱۵ مگابایت است.</span>
+      <span
+        style="text-align: center; color: gray; display: block"
+        class="mt-2 mb-4"
+        >حداکثر سایز موسیقی ۱۵ مگابایت است.</span
+      >
     </div>
     <v-row no-gutters>
       <v-col cols="6" xl="6" lg="6" md="6" sm="6" class="d-flex justify-center">
@@ -39,7 +58,7 @@
           @click="$emit('update:stepper', 5)"
           color="error"
           outlined
-           class="w-98"
+          class="w-98"
           >مرحله‌ی قبلی</v-btn
         >
       </v-col>
@@ -54,9 +73,7 @@
         >
         <v-btn
           v-if="wantMusic"
-          :disabled="
-            mp3 === null || !musicForm || done
-          "
+          :disabled="mp3 === null || !musicForm || done"
           :color="done ? 'green' : 'primary'"
           class="white--text w-98"
           @click="uploadMusic"
@@ -66,7 +83,11 @@
         </v-btn>
       </v-col>
       <v-col cols="12" xl="12" lg="12" md="12" sm="12" class="my-3">
-        <v-btn v-if="(done || !musicForm) && wantMusic" color="primary" @click="checkout" block
+        <v-btn
+          v-if="(done || !musicForm) && wantMusic"
+          color="primary"
+          @click="checkout"
+          block
           >نهایی سازی سفارش</v-btn
         >
       </v-col>
@@ -87,7 +108,7 @@ export default {
     return {
       musicForm: false,
       rules: [
-        value => !value || value.size < 15000000 || 'این فایل مجاز نیست!',
+        (value) => !value || value.size < 15000000 || "این فایل مجاز نیست!",
       ],
       loading: false,
       wantMusic: false,
@@ -123,9 +144,10 @@ export default {
               alert("خطایی پیش آمده است...");
             }
             this.loading = false;
-          }).catch(error=>{
+          })
+          .catch((error) => {
             this.loading = false;
-            console.log(error)
+            console.log(error);
           });
       } else {
         this.loading = false;
@@ -162,8 +184,32 @@ export default {
         this.$router.push("/pay");
       }
     },
+    /**
+     * Determine the mobile operating system.
+     * This function returns one of 'iOS', 'Android', 'Windows Phone', or 'unknown'.
+     *
+     * @returns {String}
+     */
+    getMobileOperatingSystem() {
+      var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+      // Windows Phone must come first because its UA also contains "Android"
+      if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+      }
+
+      if (/android/i.test(userAgent)) {
+        return "Android";
+      }
+
+      // iOS detection from: http://stackoverflow.com/a/9039885/177710
+      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+      }
+
+      return "unknown";
+    },
   },
 };
 </script>
 
-<style></style>
